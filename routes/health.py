449dc8b967aa7acea7f6057e5c1d94595cf558f0
limberg05 <hernandezdelriojuan@gmail.com>
2025-12-07 22:47:38 -0600

    prueba ordionario

--- routes/health.py
+++ routes/health.py
@@ -7,7 +7,7 @@ health_bp = Blueprint('health', __name__)
 def health_check():
     try:
         return jsonify({
-            "message": "Server is automticly",
+            "message": "Server is on line now",
             "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         }), 200
     except Exception as e:
